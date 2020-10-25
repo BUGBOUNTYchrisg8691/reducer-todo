3b12203db543d2742e0bddad0b601fe1c7d337fe
Christopher Girvin <bugbounty.chrisg8691@gmail.com>
2020-10-25 14:07:06 -0400

    react app(webpack, babel) and eslint setup/gitignore update

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+  return <div>React with webpack</div>;
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
